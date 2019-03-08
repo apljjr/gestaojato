cb36c096095b1ff4e3f31890905cc873aa52423e
Adeilson Pereira <aplj.adeilson@gmail.com>
2019-03-07 23:30:12 -0300

    Inlcuindo classe utilitaria para status

---
+++ gestaojato-api/src/main/java/br/com/gestaojato/util/Status.java
@@ -0,0 +1,10 @@
+package br.com.gestaojato.util;
+
+public final class Status {
+
+    public static String AGUARDANDO_ATENDIMENTO = "aguardando";
+    public static String EM_ATENDIMENTO = "progresso";
+    public static String ATENDIMENTO_FINALIZADO = "finalizado";
+    public static String ATENDIMENTO_CANCELADO = "cancelado";
+
+}
